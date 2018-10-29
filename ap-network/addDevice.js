77718a03b9aa98b8c0a41fe752a08e42e1ffe483
HardPlant <abc7988se@naver.com>
2018-10-29 11:25:24 +0900

    update

---
+++ ap-network/addDevice.js
@@ -0,0 +1,15 @@
+const exec = require("child_process").exec;
+const fs = require('fs')
+
+const command = "composer participant add -c admin@ap-network"
+for(var i=1; i<5;i++){
+    var json = fs.readFileSync(`device${i}.json`)
+    var data = JSON.stringify(JSON.parse(json))
+    console.log(data)
+    // exec(`${command} -d ${data}`, (error, stdout, stderr)=>{
+    //     console.log(`error: ${error}`)
+    //     console.log(`stdout: ${stdout}`)
+    //     console.log(`stderr: ${stderr}`)
+    // })
+}
+process.exit(0)
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,11 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import AxiosTest from '@/components/AxiosTest'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld
+    },
+    {
+      path: '/test',
+      name: 'AxiosTest',
+      component: AxiosTest
+    },
     {
       path: '/',
       name: 'HelloWorld',
