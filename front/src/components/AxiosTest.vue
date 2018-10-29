d23634d13475c0b0ea65a4e8313e017dd15a4e5c
HardPlant <abc7988se@naver.com>
2018-10-29 09:35:36 +0900

    update

---
+++ front/src/components/AxiosTest.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="ax">
+        <button v-on:click="getDeviceList">장비 목록 보기</button>
+        <button v-on:click="registerDevice">장비 등록</button>
+    </div>
+</template>
+
+<script>
+const baseURI= 'https://';
+
+export default {
+    name: 'ax',
+    data: function(){
+        return {
+            devices : []
+        }
+    },
+    methods: {
+        getDeviceList: ()=>{
+            this.$http.get(`${baseURI}/`).then(
+                (result)=>{
+
+                })
+        },
+        registerDevice: ()=>{
+            this.$http.post(`${baseURI}/`).then(
+                (result)=>{
+
+                })
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
