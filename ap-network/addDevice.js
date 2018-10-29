6e54f6cb75efd76df16fe7cf19370fa7d24089b9
HardPlant <abc7988se@naver.com>
2018-10-29 14:16:35 +0900

    update

--- ap-network/addDevice.js
+++ ap-network/addDevice.js
@@ -2,14 +2,14 @@ const exec = require("child_process").exec;
 const fs = require('fs')
 
 const command = "composer participant add -c admin@ap-network"
-for(var i=1; i<5;i++){
+for(var i=1; i<=5;i++){
     var json = fs.readFileSync(`device${i}.json`)
     var data = JSON.stringify(JSON.parse(json))
     console.log(data)
-    // exec(`${command} -d ${data}`, (error, stdout, stderr)=>{
-    //     console.log(`error: ${error}`)
-    //     console.log(`stdout: ${stdout}`)
-    //     console.log(`stderr: ${stderr}`)
-    // })
+    exec(`${command} -d '${data}'`, (error, stdout, stderr)=>{
+        console.log(`error: ${error}`)
+        console.log(`stdout: ${stdout}`)
+        console.log(`stderr: ${stderr}`)
+    })
 }
 process.exit(0)
--- front/src/components/AxiosTest.vue
+++ front/src/components/AxiosTest.vue
@@ -1,33 +1,74 @@
 <template>
     <div id="ax">
+        <p v-bind="message">{{message}}</p>
         <button v-on:click="getDeviceList">장비 목록 보기</button>
-        <button v-on:click="registerDevice">장비 등록</button>
+        <button v-on:click="registerDevice">연결 트랜잭션</button>
+        <button v-on:click="registerDevice">연결 해제 트랜잭션</button>
     </div>
 </template>
 
 <script>
-const baseURI= 'https://';
+const baseURI= 'http://localhost:3000/api';
+import axios from 'axios'
 
 export default {
     name: 'ax',
     data: function(){
         return {
+            message: "",
             devices : []
         }
     },
     methods: {
-        getDeviceList: ()=>{
-            this.$http.get(`${baseURI}/`).then(
+        getDeviceList: function(){
+            this.$http.get(`${baseURI}/net.ap.Device`).then(
                 (result)=>{
+                    this.devices=result["data"];
+                    this.message=this.devices[0]["DeviceID"];
+                })
+        },
+        setDeviceList: function(){
+            this.$http.post(`${baseURI}/`).then(
+                (result)=>{
+                    this.message=result;
 
                 })
         },
-        registerDevice: ()=>{
+        registerDevice: function(){
             this.$http.post(`${baseURI}/`).then(
                 (result)=>{
+                    this.message=result;
 
                 })
-        }
+        },
+        connectDevice: function(){
+            var transaction = {
+                "$class": "net.ap.registerDevice",
+                "newDeviceMAC": "string",
+                "time": "string",
+                "deviceFrom": "string",
+                "transactionId": "string",
+                "timestamp": "2018-10-29T04:48:31.437Z"
+                }
+            this.$http.post(`${baseURI}/`).then(
+                (result)=>{
+                    this.message=result;
+                
+                })
+        },
+        getConnectionTransaction: function(){
+            this.$http.post(`${baseURI}/`).then(
+                (result)=>{
+                    this.message=result;
+
+                })
+        },
+        disconnectDevice: function(){
+            this.$http.post(`${baseURI}/`).then(
+                (result)=>{
+                    this.message=result;
+                })
+        },
     }
 }
 </script>
